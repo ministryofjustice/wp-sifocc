b7c74fcd64363b6388837363a3e918e143c3993d
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-03 14:17:48 +0000

    Framework Cleanup - Moved WP Head Clean to setup

--- web/app/themes/sifocc-theme/inc/setup.php
+++ web/app/themes/sifocc-theme/inc/setup.php
@@ -45,3 +45,24 @@ if (!function_exists('sifocc_setup')) {
 }
 
 add_action('after_setup_theme', 'sifocc_setup');
+
+if (!function_exists('sifocc_clean_wphead')) {
+
+    function sifocc_clean_wphead()
+    {
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_styles', 'print_emoji_styles');
+        remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        // Remove extra crap from wp_head
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wp_generator');
+        remove_action('wp_head', 'wlwmanifest_link');
+        remove_action('wp_head', 'feed_links_extra', 3);
+        remove_action('wp_head', 'start_post_rel_link', 10, 0);
+        remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+        remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+    }
+}
+
+add_action('init', 'sifocc_clean_wphead');
