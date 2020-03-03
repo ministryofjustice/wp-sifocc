0b7f3a97cf59e4ff888875333f161c8d7061d462
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-03-03 20:30:01 +0000

    Framework Cleanup - Remove last files of Iguana Framework

--- web/app/themes/sifocc-theme/app/di.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$registrar->addInstance(\Dxw\Iguana\Theme\Helpers::class, new \Dxw\Iguana\Theme\Helpers());
-$registrar->addInstance(\Dxw\Iguana\Theme\LayoutRegister::class, new \Dxw\Iguana\Theme\LayoutRegister(
-    $registrar->getInstance(\Dxw\Iguana\Theme\Helpers::class)
-));
